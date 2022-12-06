b902fcad2212b2d850a5a9edfe1ca5bd240aafdb
Nithis8866 <119437049+Nithis8866@users.noreply.github.com>
2022-12-06 11:38:47 +0530

    Add files via upload

---
+++ EmployeeDetails.java
@@ -0,0 +1,20 @@
+package week2.day1;
+
+public class EmployeeDetails 
+{
+	String empName="Nithis";
+	int empId=732;
+	String empAddress="chennai";
+	double empSalary=36899.88;
+	long empPhoneNumber=846372857l;
+	public static void main(String[] args) {
+		EmployeeDetails emp=new EmployeeDetails();
+		System.out.println(emp.empName);
+		System.out.println(emp.empId);
+		System.out.println(emp.empAddress);
+		System.out.println(emp.empSalary);
+		System.out.println(emp.empPhoneNumber);
+	}
+	
+   
+}
---
+++ PositiveOrNegative.java
@@ -0,0 +1,21 @@
+package week2.day1;
+
+public class PositiveOrNegative {
+	
+public static void main(String[] args) {
+		int number=35;
+		if(number>0)
+		{
+			System.out.println("The number is positive");
+		}
+	if (number<0)
+		{
+			System.out.println("The number is negative");
+		}
+ else
+		{
+			System.out.println("The number is neither positive nor negative");
+		}
+		
+	}
+}
